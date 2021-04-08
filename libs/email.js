b5d54514459ce92a05f85adbc0c019be28f79c57
Richard <rich_chen@hpicorp.com.tw>
2021-04-08 11:27:25 +0800

    test finished

--- libs/email.js
+++ libs/email.js
@@ -5,14 +5,13 @@ module.exports = function (to) {
   const msg = {
     to,
     from: 'td231565@gmail.com',
-    subject: 'Congrate to sign up!',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    subject: '測試 SendGrid 寄信功能',
+    text: `這是一封從 ${process.env.PORT ? 'localhost' : 'Heroku'} 寄出的測試信`,
+    html: `<strong>這是一封從 localhost 寄出的測試信，收件人是 ${to}</strong>`,
   }
   sgMail
     .send(msg)
     .then(res => {
-      console.log(res)
       console.log('Email sent')
     })
     .catch((error) => {
